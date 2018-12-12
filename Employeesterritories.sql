8d8c79d11fc2be618ea04c1c3934d932f9362edd
Aaashi <45806087+Aaashi@users.noreply.github.com>
2018-12-11 20:32:41 -0500

    Add files via upload

---
+++ Employeesterritories.sql
@@ -0,0 +1,87 @@
+create Multiset Table Workspace.EmployeesTerritories 
+(
+   EmployeeID int not null,
+   TerritoryID int not null)
+   primary index(EmployeeID,TerritoryID);
+   
+   select * from Workspace.EmployeesTerritories;
+   
+   INSERT INTO Workspace.EmployeesTerritories VALUES(1, '06897');
+
+INSERT INTO Workspace.EmployeesTerritories VALUES(1, '19713');
+
+INSERT INTO Workspace.EmployeesTerritories VALUES(2, '01581');
+
+INSERT INTO Workspace.EmployeesTerritories VALUES(2, '01730');
+
+INSERT INTO Workspace.EmployeesTerritories VALUES(2, '01833');
+
+INSERT INTO Workspace.EmployeesTerritories VALUES(2, '02116');
+
+INSERT INTO Workspace.EmployeesTerritories VALUES(2, '02139');
+
+INSERT INTO Workspace.EmployeesTerritories VALUES(2, '02184');
+
+INSERT INTO Workspace.EmployeesTerritories VALUES(2, '40222');
+
+INSERT INTO Workspace.EmployeesTerritories VALUES(3, '30346');
+
+INSERT INTO Workspace.EmployeesTerritories VALUES(3, '31406');
+
+INSERT INTO Workspace.EmployeesTerritories VALUES(3, '32859');
+
+INSERT INTO Workspace.EmployeesTerritories VALUES(3, '33607');
+
+INSERT INTO Workspace.EmployeesTerritories VALUES(4, '20852');
+
+INSERT INTO Workspace.EmployeesTerritories VALUES(4, '27403');
+
+INSERT INTO Workspace.EmployeesTerritories VALUES(4, '27511');
+
+INSERT INTO Workspace.EmployeesTerritories VALUES(5, '02903');
+
+INSERT INTO Workspace.EmployeesTerritories VALUES(5, '07960');
+
+INSERT INTO Workspace.EmployeesTerritories VALUES(5, '08837');
+
+INSERT INTO Workspace.EmployeesTerritories VALUES(5, '10019');
+
+INSERT INTO Workspace.EmployeesTerritories VALUES(5, '10038');
+
+INSERT INTO Workspace.EmployeesTerritories VALUES(5, '11747');
+
+INSERT INTO Workspace.EmployeesTerritories VALUES(5, '14450');
+
+INSERT INTO Workspace.EmployeesTerritories VALUES(6, '85014');
+
+INSERT INTO Workspace.EmployeesTerritories VALUES(6, '85251');
+
+INSERT INTO Workspace.EmployeesTerritories VALUES(6, '98004');
+
+INSERT INTO Workspace.EmployeesTerritories VALUES(6, '98052');
+
+INSERT INTO Workspace.EmployeesTerritories VALUES(6, '98104');
+
+INSERT INTO Workspace.EmployeesTerritories VALUES(8, '19428');
+
+INSERT INTO Workspace.EmployeesTerritories VALUES(8, '44122');
+
+INSERT INTO Workspace.EmployeesTerritories VALUES(8, '45839');
+
+INSERT INTO Workspace.EmployeesTerritories VALUES(8, '53404');
+
+INSERT INTO Workspace.EmployeesTerritories VALUES(9, '03049');
+
+INSERT INTO Workspace.EmployeesTerritories VALUES(9, '03801');
+
+INSERT INTO Workspace.EmployeesTerritories VALUES(9, '48075');
+
+INSERT INTO Workspace.EmployeesTerritories VALUES(9, '48084');
+
+INSERT INTO Workspace.EmployeesTerritories VALUES(9, '48304');
+
+INSERT INTO Workspace.EmployeesTerritories VALUES(9, '55113');
+
+INSERT INTO Workspace.EmployeesTerritories VALUES(9, '55439');
+
+select * from Workspace.EmployeesTerritories;
